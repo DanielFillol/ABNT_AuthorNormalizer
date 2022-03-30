403f8fd938583eecbcc190afb565e3034879900c
fillol <dniel_fillol@hotmail.com>
2022-03-30 17:47:21 -0300

    First commit

---
+++ ABNT.go
@@ -0,0 +1,88 @@
+package Author
+
+import (
+	"fmt"
+	"github.com/Darklabel91/ABNT_AuthorNormalizer/CSV"
+	"github.com/Darklabel91/ABNT_AuthorNormalizer/Functions"
+	"github.com/Darklabel91/ABNT_AuthorNormalizer/Structs"
+	"strings"
+)
+
+func AbntFormat(name string) (string, string, string) {
+	var abnt string
+	var textABNTSmall string
+	var textABNTLong string
+
+	var finalName string
+	var sonName string
+	var initialNames []string
+	var initialLetters []string
+
+	splitNames, qtdNames := Functions.SplitName(name)
+
+	lastName := splitNames[qtdNames-1]
+
+	if Functions.JuniorName(lastName) == true {
+		finalName = splitNames[qtdNames-2]
+		sonName = splitNames[qtdNames-1]
+	} else {
+		finalName = lastName
+		sonName = ""
+	}
+
+	for i := 0; i < len(splitNames); i++ {
+		if Functions.Preposition(splitNames[i]) == false && splitNames[i] != finalName && splitNames[i] != sonName {
+			initialLetters = append(initialLetters, splitNames[i][0:1]+". ")
+			initialNames = append(initialNames, splitNames[i]+" ")
+		} else if splitNames[i] != finalName && splitNames[i] != sonName {
+			initialNames = append(initialNames, splitNames[i]+" ")
+		}
+	}
+
+	if sonName != "" {
+		abnt = strings.ToUpper(finalName) + " " + strings.ToUpper(sonName) + ", "
+	} else {
+		abnt = strings.ToUpper(finalName) + ", "
+	}
+
+	textABNTSmall = abnt
+	textABNTLong = abnt
+
+	for i := 0; i < len(initialLetters); i++ {
+		textABNTSmall += initialLetters[i]
+	}
+
+	for i := 0; i < len(initialNames); i++ {
+		textABNTLong += initialNames[i]
+	}
+
+	textABNTnoDot := strings.Replace(textABNTSmall, ".", "", -1)
+
+	return textABNTLong, textABNTnoDot, textABNTSmall
+}
+
+func DocClassifierCSV(rawFilePath string, separator rune, nameResultFolder string) {
+	raw := CSV.ReadCsvFile(rawFilePath, separator)
+	createCSVs(raw, nameResultFolder)
+	fmt.Println("Files created")
+}
+
+func createCSVs(raw []string, nameResultFolder string) []Structs.DataABNT {
+	var authorsABNT []Structs.DataABNT
+
+	for i := 0; i < len(raw); i++ {
+		textL, textD, textS := AbntFormat(raw[i])
+
+		data := Structs.DataABNT{
+			AuthorName:    raw[i],
+			TextABNTLong:  textL,
+			TextABNTnoDot: textD,
+			TextABNTSmall: textS,
+		}
+
+		authorsABNT = append(authorsABNT, data)
+	}
+
+	return authorsABNT
+
+}
